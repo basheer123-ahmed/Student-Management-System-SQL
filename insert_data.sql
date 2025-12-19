6ffa5a1bf90d057adac16b2c91762125f8950f60
BasheerAhmed141 <basheerm2006@gmail.com>
2025-12-19 17:08:46 +0530

    Initial SQL project: Student Management System

---
+++ insert_data.sql
@@ -0,0 +1,18 @@
+
+INSERT INTO STUDENT(student_id, name, age, city) VALUES
+(1, 'Basheer', 21, 'Hyderabad'),
+(2, 'Ali', 20, 'Vijayawada'),
+(3, 'Rahul', 22, 'Bangalore'),
+(4, 'Sneha', 19, 'Chennai');
+
+INSERT INTO COURSE VALUES
+(101, 'Java', 'Dr.Ramesh'),
+(102, 'Python', 'Dr.Suresh'),
+(103, 'AI', 'Dr.Kiran');
+
+INSERT INTO ENROLLMENT VALUES
+(1, 1, 101, 85),
+(2, 2, 101, 78),
+(3, 3, 102, 90),
+(4, 4, 103, 88),
+(5, 1, 102, 92);
